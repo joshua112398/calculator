276d484ec9c4d9fb842de6c542f9cfc9eda624a7
Josh Geronimo <joshuageronimo112398@gmail.com>
2022-05-12 04:30:15 -0700

    Implement complete functionality of calculator
    Allow chaining of mathematical operations, clearing of display and
    data, and use of decimal values. Various bug fixes.

--- scripts/script.js
+++ scripts/script.js
@@ -26,12 +26,12 @@ function operate(operator, a, b) {
       return add(a,b);
     case '-':
       return subtract(a,b);
-    case '*':
+    case 'x':
       return multiply(a,b);
     case '/':
       return divide(a,b);
     default:
-      return "Invalid Operator";
+      return 0;
   }
 }
 
@@ -45,19 +45,120 @@ function displayInput(a) {
 
 //////////////////////////////////////////////////////
 
+function clearDisplay() {
+  const display = document.querySelector(".display-text");
+  display.textContent = "";
+}
 
+////////////////////////////d//////////////////////////
+
+function clearData(obj) {
+  obj.firstNumber = 0;
+  obj.secondNumber = 0;
+  obj.operator = "";
+  return obj;
+}
 
 //////////////////////////////////////////////////////
 
-function keyPress() {
+function ifEqualsInput(obj) {
+  if (obj.operator === "") {
+    return obj;
+  }
+  const display = document.querySelector(".display-text");
+  obj.secondNumber = +(display.textContent);
+  let solution = operate(obj.operator, obj.firstNumber, 
+      obj.secondNumber);
+  solution = solution;
+  clearDisplay();
+  displayInput(solution);
+  obj = clearData(obj);
+  // place the solution as the first number, in case
+  // user wants to do further calculations with
+  // the result
+  obj.firstNumber = solution;
+  console.log(obj);
+  return obj;
+}
+//////////////////////////////////////////////////////
+
+function ifOperatorInput(obj, operatorButton, lastButton) {
+  if (lastButton === "+" || lastButton === "-" || lastButton === "x" ||
+      lastButton === "/") {
+    obj.operator = operatorButton;
+    return obj;
+  }
+  // Two cases: if previous operator was empty,
+  // which only happens after pressing '=' or
+  // after pressing CLEAR
+  if (obj.operator === "") {
+    const display = document.querySelector(".display-text");
+    obj.firstNumber = +(display.textContent);
+    obj.operator = operatorButton.textContent;
+    console.log(obj);
+    return obj;
+  }
+  // If previous operator was also +,-,x, or /, then find its
+  // solution, then use the solution as the first number for a new
+  // operation, with the new operator being the operator inputted
+  // to this function.
+  else {
+    obj = ifEqualsInput(obj);
+    obj.operator = operatorButton.textContent;
+    return obj;
+  }
+}
+
+//////////////////////////////////////////////////////
+
+function onKeyPress() {
+  let equation = {
+    firstNumber: 0,
+    secondNumber: 0,
+    operator: "",
+  };
+
+  displayInput(0);
+
+  let lastClicked = "";
+
   const buttons = document.querySelectorAll('button');
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
 
       if (button.getAttribute("class") === "number") {
+        const display = document.querySelector(".display-text");
+        if (display.textContent === "0") {
+          clearDisplay();
+        } 
+        if (lastClicked === "=") {
+          clearDisplay();
+          equation = clearData(equation);
+        }
+        else if (lastClicked === "/" ||lastClicked === "x" ||
+            lastClicked === "-" ||lastClicked === "+" ) {
+          clearDisplay();
+        }
         displayInput(button.textContent);
-      } else if (button.getAttribute("class") === "operator") {
+        lastClicked = button.textContent;
 
+      } else if (button.getAttribute("class") === "operator") {
+        equation = ifOperatorInput(equation, button, lastClicked);
+        lastClicked = button.textContent;
+
+      } else if (button.textContent === "=") {
+        if (lastClicked !== "/" &&lastClicked !== "x" &&
+            lastClicked !== "-" &&lastClicked !== "+" ) {
+          equation = ifEqualsInput(equation);
+          lastClicked = button.textContent;
+        }
+
+      } else {
+        clearDisplay();
+        displayInput(0);
+        equation = clearData(equation);
+        lastClicked = "";
+        console.log(equation);
       }
 
     })
@@ -67,8 +168,6 @@ function keyPress() {
 //////////////////////////////////////////////////////
 
 (() => {
-  let firstNumber;
-  let secondNumber;
-  keyPress();
+  onKeyPress();
 
 })();
